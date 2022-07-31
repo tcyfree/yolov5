ebf68390447f79b5ba5ce2204cfa40b06e3b9834
tangcy <bohr@icloud.com>
2022-07-31 16:23:54 +0800

    cc

---
+++ front-end/src/components/Header.vue
@@ -0,0 +1,75 @@
+<template>
+  <div id="Header">
+    <div id="word">
+      <h1>{{ msg }}</h1>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "Header",
+  data() {
+    return {
+      msg: "桑蚕疾病监测",
+      activeIndex: "1",
+    };
+  },
+  methods: {},
+};
+</script>
+<style scoped>
+#Header {
+  padding-top: 30px;
+  width: 100%;
+  margin: 10px auto;
+}
+
+#word {
+  margin-bottom: 37px;
+  margin-right: 110px;
+  height: 60px;
+  line-height: 3.2em;
+  letter-spacing: 8px;
+  text-align: center;
+}
+
+h1 {
+  /*text-align: center;*/
+  color: #21b3b9;
+  letter-spacing: 30px;
+  font-size: 2.3em;
+}
+
+.el-menu-demo {
+  width: 80%;
+  margin: 0px auto;
+  padding: 0px auto;
+}
+
+.top-left-edition span i {
+  float: left;
+  margin-right: 10px;
+}
+
+i,
+input,
+label {
+  vertical-align: middle;
+}
+
+i {
+  border: 0;
+  display: block;
+  cursor: pointer;
+}
+
+.top-left-edition span {
+  float: left;
+  font-size: 16px;
+  color: #999999;
+  line-height: 24px;
+  margin-right: 40px;
+}
+</style>
+
+
---
+++ front-end/vue.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  lintOnSave: undefined,
+  publicPath: './',
+  outputDir: undefined,
+  assetsDir: undefined,
+  runtimeCompiler: undefined,
+  productionSourceMap: undefined,
+  parallel: false,
+  css: undefined,
+  // 跳过检查host
+  devServer: { disableHostCheck: true }
+}
